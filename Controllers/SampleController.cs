8f1ff9ae73e519c2911854c365a550f1da6ac839
hammad3082 <hammad3082@gmail.com>
2025-09-18 06:44:59 +0530

    feat: add JWT authentication and authorization setup with Swagger support

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("public")]
+        public IActionResult PublicEndpoint()
+        {
+            return Ok("This is a public endpoint");
+        }
+
+        [Authorize]
+        [HttpGet("protected")]
+        public IActionResult ProtectedEndpoint()
+        {
+            var username = User.Identity?.Name;
+            return Ok($"Hello {username}, you accessed a protected endpoint");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SecureAuthDemo.Data;
 using SecureAuthDemo.Middleware;
 using SecureAuthDemo.Repositories;
 using SecureAuthDemo.Services;
 using Serilog;
 using System;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,9 +27,54 @@ builder.Host.UseSerilog();
 //builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); 
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer {your JWT token}'"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Authorization (placeholder)
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("tANYFuPaKNR0yGvcTmBru1999lWiapAH")),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 //builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
